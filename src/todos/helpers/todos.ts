52a832f2c29c0de7f84c088b6057ab73a513d844
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 20:35:41 +0200

    add updateTodo

---
+++ src/todos/helpers/todos.ts
@@ -0,0 +1,15 @@
+import { Todo } from '@prisma/client';
+
+export const updateTodo = async (id: string, done: boolean): Promise<Todo> => {
+  const body = { done };
+
+  const todo = await fetch(`/api/todos/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((resp) => resp.json());
+
+  return todo;
+};
