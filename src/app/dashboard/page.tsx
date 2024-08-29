19fbe138be8b9669122569c9cab65c8d9a6e4344
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 19:18:58 +0200

    add DashboardPage

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { WidgetItem } from '../../components';
+
+export default function DashboardPage() {
+  return (
+    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      <WidgetItem />
+    </div>
+  );
+}
