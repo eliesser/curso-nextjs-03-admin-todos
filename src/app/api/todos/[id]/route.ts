747ea851b3afc373e043af67a74f019dcd13f3e0
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 18:12:00 +0200

    add endpoint updateTodo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,6 +1,9 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+import { boolean, object, string } from 'yup';
 
 import prisma from '../../../lib/prisma';
+import { Todo } from '@prisma/client';
 
 interface Segments {
   params: {
@@ -8,12 +11,16 @@ interface Segments {
   };
 }
 
-export async function GET(request: Request, { params }: Segments) {
-  const todo = await prisma.todo.findUnique({
+const getTodo = async (id: string): Promise<Todo | null> => {
+  return await prisma.todo.findUnique({
     where: {
-      id: params.id,
+      id,
     },
   });
+};
+
+export async function GET(request: Request, { params }: Segments) {
+  const todo = await getTodo(params.id);
 
   if (!todo)
     return NextResponse.json(
@@ -22,4 +29,36 @@ export async function GET(request: Request, { params }: Segments) {
     );
 
   return NextResponse.json(todo);
+}
+
+const postSchema = object({
+  description: string().optional(),
+  done: boolean().optional(),
+});
+
+export async function PUT(request: NextRequest, { params }: Segments) {
+  const todo = await getTodo(params.id);
+
+  if (!todo)
+    return NextResponse.json(
+      { message: `Todo width id '${params.id}' not found` },
+      { status: 404 }
+    );
+
+  try {
+    const { description, done } = await postSchema.validate(
+      await request.json()
+    );
+
+    const todo = await prisma.todo.update({
+      where: {
+        id: params.id,
+      },
+      data: { description, done },
+    });
+
+    return NextResponse.json(todo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
