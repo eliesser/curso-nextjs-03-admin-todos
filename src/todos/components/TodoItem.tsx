67f7b7a9af89c00b758ba8f0a69fe76d2300a05b
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 20:36:15 +0200

    add parameter toggleTodo to TodoItem component

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,18 +1,25 @@
+'use client';
+
+import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
+
 import { Todo } from '@prisma/client';
 
 import styles from './TodoItem.module.css';
-import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 
 interface Props {
   todo: Todo;
+  toggleTodo: (id: string, done: boolean) => Promise<Todo | void>;
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
   return (
     <div className={todo.done ? styles.todoDone : styles.todoPending}>
-      <div className='flex flex-col sm:flex-row justify-start items-center gap-4'>
+      <div
+        className='flex flex-col sm:flex-row justify-start items-center gap-4 cursor-pointer'
+        onClick={() => toggleTodo(todo.id, !todo.done)}
+      >
         <div
-          className={`flex items-center gap-2 p-2 rounded-md cursor-pointer hover:bg-opacity-60 ${
+          className={`flex items-center gap-2 p-2 rounded-md hover:bg-opacity-60 ${
             todo.done ? 'bg-blue-100' : 'bg-red-100'
           }`}
         >
