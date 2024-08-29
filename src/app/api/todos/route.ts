0273aa55f2ecdb550e247e1dc73d3886b5fe5e30
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 17:20:15 +0200

    add pagination to endpoint getTodos

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,9 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import prisma from '../../lib/prisma';
 
-export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany();
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  const take = Number(searchParams.get('take') ?? '10');
+  const skip = Number(searchParams.get('skip') ?? '0');
+
+  if (isNaN(take))
+    return NextResponse.json(
+      { message: 'Take must be a number' },
+      { status: 400 }
+    );
+
+  if (isNaN(skip))
+    return NextResponse.json(
+      { message: 'Skip must be a number' },
+      { status: 400 }
+    );
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
 
   return NextResponse.json(todos);
 }
