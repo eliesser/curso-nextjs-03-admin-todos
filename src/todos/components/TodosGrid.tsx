3b64452676a9966d54b4fe424efe06bd5306f07a
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 20:36:50 +0200

    call updateTodo and refresh page

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,17 +1,31 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { Todo } from '@prisma/client';
 import { TodoItem } from './TodoItem';
+
+import * as todosApi from '@/todos/helpers/todos';
 
 interface Props {
   todos?: Todo[];
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
+  const router = useRouter();
+
+  const toggleTodo = async (id: string, done: boolean) => {
+    const updateTodo = await todosApi.updateTodo(id, done);
+
+    router.refresh();
+
+    return updateTodo;
+  };
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-3 gap-2'>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
       ))}
     </div>
   );
