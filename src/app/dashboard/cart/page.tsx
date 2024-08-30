af1045430485b8dc815614991f5db57af7965554
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 12:59:28 +0200

    total to pay

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -4,6 +4,7 @@ import { cookies } from 'next/headers';
 import { ItemCard } from '@/shopping-cart/components/ItemCard';
 import type { Product } from '@/products/data/products';
 import { products } from '@/products/data/products';
+import { WidgetItem } from '@/components';
 
 export const metadata: Metadata = {
   title: 'Cart Page',
@@ -40,6 +41,11 @@ export default function CartPage() {
   const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}');
   const productsQuantity = getProductQuantity(cart);
 
+  const totalToPay = productsQuantity.reduce(
+    (prev, current) => current.product.price * current.quantity + prev,
+    0
+  );
+
   return (
     <div className=''>
       <h1 className='text-5xl'>Products in cart</h1>
@@ -53,6 +59,19 @@ export default function CartPage() {
               quantity={productQuantity.quantity}
             />
           ))}
+        </div>
+
+        <div className='flex flex-col w-full sm:w-4/12'>
+          <WidgetItem title='Total to paid'>
+            <div className='mt-2 flex flex-col justify-center items-center gap-4'>
+              <h3 className='text-3xl font-bold text-gray-700'>
+                ${(totalToPay * 1.15)?.toFixed(2)}
+              </h3>
+              <span className='font-bold text-center'>
+                Taxes 15%: ${(totalToPay * 0.15)?.toFixed(2)}
+              </span>
+            </div>
+          </WidgetItem>
         </div>
       </div>
     </div>
