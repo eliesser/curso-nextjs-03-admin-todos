a256ea4b6bb054cc7250abfc706bc6f9d5b5448c
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 14:15:34 +0200

    set user info

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
+import { getServerSession } from 'next-auth';
 import { CiLogout } from 'react-icons/ci';
-
-import { SidebarItem } from './SidebarItem';
 import {
   IoBasketOutline,
   IoCalculatorOutline,
@@ -11,6 +11,9 @@ import {
   IoCodeWorkingOutline,
   IoListOutline,
 } from 'react-icons/io5';
+
+import { SidebarItem } from './SidebarItem';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const sidebarItems = [
   {
@@ -40,7 +43,15 @@ const sidebarItems = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect('/api/auth/signin');
+
+  const userName = session?.user?.name ?? '';
+  const userImage =
+    session?.user?.image ?? 'https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg';
+
   return (
     <aside className='ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
       <div>
@@ -58,15 +69,13 @@ export const Sidebar = () => {
 
         <div className='mt-8 text-center'>
           <Image
-            src='https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp'
+            src={userImage}
             alt=''
             className='w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28'
             width={112}
             height={112}
           />
-          <h5 className='hidden mt-4 text-xl font-semibold text-gray-600 lg:block'>
-            Cynthia J. Watts
-          </h5>
+          <h5 className='hidden mt-4 text-xl font-semibold text-gray-600 lg:block'>{userName}</h5>
           <span className='hidden text-gray-400 lg:block'>Admin</span>
         </div>
 
