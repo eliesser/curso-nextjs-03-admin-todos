9779d3307996c44b86724967efe8f8b3d1d5c5a2
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 16:49:06 +0200

    set role and id

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,12 +10,14 @@ export default async function DashboardPage() {
   if (!session) redirect('/api/auth/signin');
 
   return (
-    <div className='grid gap-6 grid-cols-1 sm:grid-cols-2'>
+    <div className='grid gap-6 grid-cols-1'>
       <WidgetItem title='User connected S-Side'>
         <div className='flex flex-col'>
           <span>{session.user?.name}</span>
           <span>{session.user?.email}</span>
           <span>{session.user?.image}</span>
+          <span>{session.user?.id}</span>
+          <span>{session.user?.roles}</span>
         </div>
       </WidgetItem>
     </div>
