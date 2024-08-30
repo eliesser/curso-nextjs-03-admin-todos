45122892f75bc1eb8d66cfe09e9836b49dc93ae5
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-30 12:40:46 +0200

    reactor TopMenu

--- src/components/ui/TopMenu.tsx
+++ src/components/ui/TopMenu.tsx
@@ -1,20 +1,22 @@
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
+import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
+
+const getTotalCount = (cart: { [id: string]: number }): number => {
+  let items = 0;
+
+  Object.values(cart).forEach((value) => {
+    items += value as number;
+  });
+
+  return items;
+};
 
 export const TopMenu = () => {
   const cookieStore = cookies();
   const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}');
-
-  const getTotalCount = () => {
-    let items = 0;
-
-    Object.values(cart).forEach((value) => {
-      items += value as number;
-    });
-
-    return items;
-  };
+  const totalItems = getTotalCount(cart);
 
   return (
     <div className='sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5'>
@@ -47,10 +49,16 @@ export const TopMenu = () => {
           <button className='flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
             <CiChat1 size={25} />
           </button>
-          <button className='p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
-            <span className='text-sm mr-2 text-blue-800 font-bold'>{getTotalCount()}</span>
+          <Link
+            href={'/dashboard/cart'}
+            className='p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'
+          >
+            {totalItems > 0 && (
+              <span className='text-sm mr-2 text-blue-800 font-bold'>{totalItems}</span>
+            )}
+
             <CiShoppingBasket size={25} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
